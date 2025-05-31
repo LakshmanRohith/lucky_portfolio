a687da6de797f81075e7178cda9db9b6c353bd6c
LakshmanRohith <lakshmansanagapalli@gmail.com>
2025-05-31 15:10:51 +0530

    Initial deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -25,7 +25,30 @@ import { componentTagger } from "lovable-tagger";
 // import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/lucky_portfolio/',  // 👈 this should match your GitHub repo name
+//   plugins: [react()],
+// })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'),
+//     },
+//   },
+//   base: '/lucky_portfolio/', // your repo name base path for GitHub Pages (adjust if needed)
+// })
+
 export default defineConfig({
-  base: '/lucky_portfolio/',  // 👈 this should match your GitHub repo name
+  base: '/lucky_portfolio/', // Your GitHub repo name
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
